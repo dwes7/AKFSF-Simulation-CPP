f1172d8998c771c501650b96578050920d9706ce
dwes7 <dwestley.1992@gmail.com>
2022-10-14 01:57:32 +0000

    Seeing a constant 3 sigma position uncertainty

--- src/kalmanfilter.cpp
+++ src/kalmanfilter.cpp
@@ -12,9 +12,9 @@
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
-constexpr double INIT_POS_STD = 0;
-constexpr double INIT_VEL_STD = 15;
-constexpr double ACCEL_STD = 0.1;
+constexpr double INIT_POS_STD = 5;
+constexpr double INIT_VEL_STD = 0;
+constexpr double ACCEL_STD = 0;
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
 
@@ -33,11 +33,27 @@ void KalmanFilter::predictionStep(double dt)
             VectorXd state = Vector4d::Zero();
             MatrixXd cov = Matrix4d::Zero();
 
+
+
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
-            state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4);
+            
+            /**
+             * x = [0, 0, 3.536, 3.536]^T
+            */
+           
+            state(0) = 0;
+            state(1) = 0;
+            state(2) = 5.0*cos(M_PI/4);
+            state(3) = 5.0*sin(M_PI/4);
 
-            setState(state);
+            cov(0,0) = INIT_POS_STD*INIT_POS_STD;
+            cov(1,1) = INIT_POS_STD*INIT_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
+
+
+            setState(state.transpose());
             setCovariance(cov);
         // ----------------------------------------------------------------------- //
     }
@@ -53,6 +69,36 @@ void KalmanFilter::predictionStep(double dt)
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
 
+        /**
+             *        | 1 0 dt 0 |
+             *        | 0 1 0 dt |
+             *   F =  | 0 0 1  0 |
+             *        | 0 0 0  1 |
+            */
+            MatrixXd F_process = Matrix4d::Zero();
+            F_process(0, 0) = 1.0;
+            F_process(0, 2) = dt;
+            F_process(1, 1) = 1.0;
+            F_process(1, 3) = dt;
+            F_process(2, 2) = 1.0;
+            F_process(3, 3) = 1.0;
+
+            state = F_process * state;
+
+
+            MatrixXd Q = Matrix2d::Zero();
+            Q(0, 0) = ACCEL_STD*ACCEL_STD;
+            Q(1, 1) = ACCEL_STD*ACCEL_STD;
+
+
+            MatrixXd L = MatrixXd::Zero(4, 2);
+            double hdt2 = 0.5*dt*dt;
+            L(0, 0) = hdt2;
+            L(1, 1) = hdt2;
+            L(2, 0) = dt;
+            L(3, 1) = dt;
+
+            cov = F_process*cov *F_process.transpose() + L*Q*L.transpose();
 
         // ----------------------------------------------------------------------- //
 
