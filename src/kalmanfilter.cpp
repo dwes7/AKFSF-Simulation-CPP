b40d906b58a249264e515fc8dfa2c52adfc5fd72
dwes7 <dwestley.1992@gmail.com>
2022-10-26 01:21:46 +0000

    Complete LKF lesson

--- src/kalmanfilter.cpp
+++ src/kalmanfilter.cpp
@@ -12,9 +12,9 @@
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
-constexpr double INIT_POS_STD = 5;
-constexpr double INIT_VEL_STD = 0;
-constexpr double ACCEL_STD = 0;
+constexpr double INIT_POS_STD = 10;
+constexpr double INIT_VEL_STD = 10;
+constexpr double ACCEL_STD = 0.0;
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
 
@@ -44,9 +44,12 @@ void KalmanFilter::predictionStep(double dt)
            
             state(0) = 0;
             state(1) = 0;
-            state(2) = 5.0*cos(M_PI/4);
-            state(3) = 5.0*sin(M_PI/4);
-
+            // Test initial state
+            // state(2) = 5.0*cos(M_PI/4);
+            // state(3) = 5.0*sin(M_PI/4);
+            // Test covariance estimate
+            state(2) = 0;
+            state(3) = 0;
             cov(0,0) = INIT_POS_STD*INIT_POS_STD;
             cov(1,1) = INIT_POS_STD*INIT_POS_STD;
             cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
@@ -70,35 +73,35 @@ void KalmanFilter::predictionStep(double dt)
         // ENTER YOUR CODE HERE
 
         /**
-             *        | 1 0 dt 0 |
-             *        | 0 1 0 dt |
-             *   F =  | 0 0 1  0 |
-             *        | 0 0 0  1 |
-            */
-            MatrixXd F_process = Matrix4d::Zero();
-            F_process(0, 0) = 1.0;
-            F_process(0, 2) = dt;
-            F_process(1, 1) = 1.0;
-            F_process(1, 3) = dt;
-            F_process(2, 2) = 1.0;
-            F_process(3, 3) = 1.0;
+         *        | 1 0 dt 0 |
+         *        | 0 1 0 dt |
+         *   F =  | 0 0 1  0 |
+         *        | 0 0 0  1 |
+        */
+        MatrixXd F_process = Matrix4d::Zero();
+        F_process(0, 0) = 1.0;
+        F_process(0, 2) = dt;
+        F_process(1, 1) = 1.0;
+        F_process(1, 3) = dt;
+        F_process(2, 2) = 1.0;
+        F_process(3, 3) = 1.0;
 
-            state = F_process * state;
-
-
-            MatrixXd Q = Matrix2d::Zero();
-            Q(0, 0) = ACCEL_STD*ACCEL_STD;
-            Q(1, 1) = ACCEL_STD*ACCEL_STD;
+        state = F_process * state;
 
 
-            MatrixXd L = MatrixXd::Zero(4, 2);
-            double hdt2 = 0.5*dt*dt;
-            L(0, 0) = hdt2;
-            L(1, 1) = hdt2;
-            L(2, 0) = dt;
-            L(3, 1) = dt;
+        MatrixXd Q = Matrix2d::Zero();
+        Q(0, 0) = ACCEL_STD*ACCEL_STD;
+        Q(1, 1) = ACCEL_STD*ACCEL_STD;
 
-            cov = F_process*cov *F_process.transpose() + L*Q*L.transpose();
+
+        MatrixXd L = MatrixXd::Zero(4, 2);
+        double hdt2 = 0.5*dt*dt;
+        L(0, 0) = hdt2;
+        L(1, 1) = hdt2;
+        L(2, 0) = dt;
+        L(3, 1) = dt;
+
+        cov = F_process*cov *F_process.transpose() + L*Q*L.transpose();
 
         // ----------------------------------------------------------------------- //
 
@@ -120,8 +123,38 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
         // Hint: You can use the constants: GPS_POS_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE 
+        // Measurement model sensitivity matrix
+        MatrixXd H = MatrixXd(2, 4);
+        H << 1,0,0,0,0,1,0,0;
 
 
+        // Measurement covarience matrix
+        Matrix2d R = Matrix2d::Zero();
+        R(0, 0) = GPS_POS_STD*GPS_POS_STD;
+        R(1, 1) = GPS_POS_STD*GPS_POS_STD;
+
+        // measurement state
+        VectorXd zk = Vector2d::Zero();
+        zk(0) = meas.x;
+        zk(1) = meas.y;
+
+        // innovation difference between current measurement and predicted sensor measurement
+        VectorXd innovation = zk - H * state;;
+
+
+        // innovation covariance
+        // This tells us how much uncertainty in the 
+        //2x4*2x2*2*4
+        Matrix2d Sk = H * cov * H.transpose() + R;
+
+        // kalman gain matrix
+        MatrixXd kalman_gain = cov * H.transpose() * Sk.inverse();
+
+        // state update step
+        state = state + kalman_gain*innovation;
+
+        // covariance update step
+        cov = (Matrix4d::Identity(4, 4) - kalman_gain * H)*cov;
         // ----------------------------------------------------------------------- //
 
         setState(state);
@@ -138,6 +171,16 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
         // ENTER YOUR CODE HERE
             VectorXd state = Vector4d::Zero();
             MatrixXd cov = Matrix4d::Zero();
+
+            // state << 0,0,0,0;
+            // cov << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;
+
+            state(0) = meas.x;
+            state(1) = meas.y;
+            cov(0,0) = GPS_POS_STD*GPS_POS_STD;
+            cov(1,1) = GPS_POS_STD*GPS_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
 
 
             setState(state);
